5c34d7b0c98cdfe00939030724e12d563c533a7b
vaibhav02012000 <vaibhav.jauhari@appinventiv.com>
2024-02-27 13:42:11 +0530

    Bug Fixes on login Page

--- Login.java
+++ Login.java
@@ -7,6 +7,8 @@ public static void main(String[] abc)
 	
 
 	System.out.println("Hello i am login page");
+	System.out.println("Hello i am login page");
+	System.out.println("Hello i am login page");
 }
 
 
